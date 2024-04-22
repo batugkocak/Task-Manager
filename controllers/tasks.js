aebff7fdc27551247df5b02d7466ad84857c7d34
Batug Kocak <batugkocak@gmail.com>
2024-04-23 02:41:58 +0300

    feat: Add MongoDB connection

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,17 +1,21 @@
-const getAllTasks = (req, res) => {
-  res.send("Get All Task");
+const Task = require("../models/task");
+
+const getAllTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.status(200).json(tasks);
 };
 
-const getTask = (req, res) => {
-  res.send("Get Single Task");
+const getTask = (req, res) => { 
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
   res.send("Update Task");
 };
 
-const createTask = (req, res) => {
-  res.send("Create Task");
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(200).json(task);
 };
 
 const deleteTask = (req, res) => {
