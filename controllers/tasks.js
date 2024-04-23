d738423a9e3d1f85996468e3e6b24072b6e7ab7a
Batug Kocak <batugkocak@gmail.com>
2024-04-23 15:26:47 +0300

    feat: Done basic controller setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,25 +1,59 @@
 const Task = require("../models/task");
 
 const getAllTasks = async (req, res) => {
-  const tasks = await Task.find();
-  res.status(200).json(tasks);
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => { 
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No tasks with id of ${taskID}` });
+    }
+    return res.status(200).json(task);
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update Task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json(task);
+  } catch (error) {}
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(200).json(task);
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete Task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete(taskID);
+    if (!task) {
+      return res.status(404).json({ msg: `No tasks with id of ${taskID}` });
+    }
+    return res.status(200).json({ deletedTask: task });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
+  return res.send("Delete Task");
 };
 
 module.exports = {
